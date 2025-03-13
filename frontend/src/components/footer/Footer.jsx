adc6aeb9793b8533ce1009615da0eaa8fe491115
phihung1902 <hungnpde170043@fpt.edu.vn>
2025-03-14 02:36:10 +0700

    update foter

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -11,8 +11,8 @@ const Footer = () => {
     <footer>
       <div className="footer-content">
         <p>
-          &copy; 2024 Your E-Learning Platform. All rights reserved. <br /> Made
-          with ❤️ <a href="">Prashant Kumar</a>
+          &copy; 2024 Your E-Learning Platform. All rights reserved. <br />{" "}
+          Contact Us : <a href="">0849431868</a>
         </p>
         <div className="social-links">
           <a href="">
